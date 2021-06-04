805ad3f219ff3e2d5b5d6a68e4c2e9371cb5670e
zema2906 <zema290613@gmail.com>
2021-06-04 23:59:26 +0300

    New

--- konkurs.js
+++ konkurs.js
@@ -1,3 +1,6 @@
-function change() {
-    document.getElementById("1").innerHTML = '<b>Выеб</b>';
+function change1() {
+    document.getElementById("1").innerHTML = '<b>Выеб </b>';
+}
+function change2() {
+    document.getElementById("2").innerHTML = '<b>города </b>';
 }
